65c00e8a88cc2b3db89dd58b601a313c3d059c21
USerHRD01 <edomarikashvili@gmail.com>
2025-02-22 15:22:07 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Donation Page Loaded!");
+
+    // Example: Change text color on click
+    document.querySelector("h1").addEventListener("click", function () {
+        this.style.color = "blue";
+    });
+});
